972f246454a071bee11dd65158f8bc369984ecb8
Satyam Kumar <sk9161511745@gmail.com>
2024-07-25 23:57:59 +0530

    Create leetcode1290.cpp
    leetcode1290

---
+++ leetcode1290.cpp
@@ -0,0 +1,32 @@
+// Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+// Return the decimal value of the number in the linked list.
+
+// The most significant bit is at the head of the linked list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+// Example 2:
+
+// Input: head = [0]
+// Output: 0
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int cnt=0;
+        ListNode* temp=head;
+        int t=0;
+        while(temp!=nullptr){
+            t=t*2+ temp->val;
+            temp=temp->next;
+        }
+        return t;
+    }
+};
