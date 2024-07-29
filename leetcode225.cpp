82eb2ddbb9b1288368280e41153276229d71cf82
Satyam Kumar <sk9161511745@gmail.com>
2024-07-29 23:22:26 +0530

    Create leetcode225.cpp
    leetcode225

---
+++ leetcode225.cpp
@@ -0,0 +1,64 @@
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+// Implement the MyStack class:
+
+// void push(int x) Pushes element x to the top of the stack.
+// int pop() Removes the element on the top of the stack and returns it.
+// int top() Returns the element on the top of the stack.
+// boolean empty() Returns true if the stack is empty, false otherwise.
+// Notes:
+
+// You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+// Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+// Example 1:
+
+// Input
+// ["MyStack", "push", "push", "top", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// Output
+// [null, null, null, 2, 2, false]
+
+// Explanation
+// MyStack myStack = new MyStack();
+// myStack.push(1);
+// myStack.push(2);
+// myStack.top(); // return 2
+// myStack.pop(); // return 2
+// myStack.empty(); // return False
+
+class MyStack {
+public:
+    queue<int> que1;
+    queue<int> que2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        que2.push(x);
+
+        while(!que1.empty()){
+            que2.push(que1.front());
+            que1.pop();
+        }
+
+        swap(que1, que2);
+    }
+    
+    int pop() {
+        int result = que1.front();
+        que1.pop();
+
+        return result;
+    }
+    
+    int top() {
+         return que1.front();
+    }
+    
+    bool empty() {
+        return que1.empty();
+    }
+};
